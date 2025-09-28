3a6b23661d17557638e89b44434988d8ce5ce032
Rip_Blue <ripblueskyrh@gmail.com>
2025-09-28 09:13:25 -0400

    Add files via upload

---
+++ Space_Invaders.py
@@ -0,0 +1,39 @@
+import math
+import random
+import pygame
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 500
+PLAYER_START_X = 370
+PLAYER_START_Y = 380
+ENEMY_START_Y_MIN = 50
+ENEMY_START_Y_MAX = 150
+ENEMY_SPEED_X = 4
+ENEMY_SPEED_Y = 40
+BULLET_SPEED_Y = 15
+COLLISION_DISTANCE = 15
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+background = pygame.image.load('backgound.png')
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+playerImg = pygame.image.load(player.png)
+playerX = PLAYER_START_X
+playerY = PLAYER_START_Y
+playerX_change = 0
+
+enemyImg = []
+enemyX = []
+enemyY = []
+enemyX_change = []
+enemyY_change= []
+num_of_enemies = 100
+
+for i in range(num_of_enemies):
+    enemyImg.append(pygame.image.load('enemy.png'))
